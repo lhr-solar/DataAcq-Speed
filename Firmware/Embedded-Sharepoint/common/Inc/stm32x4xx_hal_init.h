5d5fbdb4dd70ee111df1197462b381f63436903a
Aayush Vij <vij.aayush@gmail.com>
2025-03-29 16:14:20 -0500

    embeddedsharepoint

---
+++ Firmware/Embedded-Sharepoint/common/Inc/stm32x4xx_hal_init.h
@@ -0,0 +1,16 @@
+#ifndef STM32x4xx_HAL_INIT_H
+#define STM32x4xx_HAL_INIT_H
+
+/**
+ * @brief Initializes the Global MSP.
+ * 
+ */
+void HAL_MspInit(void);
+
+/**
+ * @brief System Clock Configuration
+ * 
+ */
+void SystemClock_Config(void);
+
+#endif /* STM32x4xx_HAL_INIT_H */
